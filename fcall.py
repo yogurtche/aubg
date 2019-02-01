7636290a425093ff6c074facc723f6fcedcd5eb6
kjkj <asddsf@dsafdsf.bg>
2019-02-01 16:55:03 +0200

    Python Commit Functions

---
+++ fcall.py
@@ -0,0 +1,12 @@
+def func1():
+    return 42
+
+
+def my_function(var1, var2):
+    return var1*var2
+
+
+print(my_function(5, 6))
+
+
+print(func1())
--- variables.py
+++ variables.py
@@ -3,5 +3,5 @@ var1 = var2 = 2
 num1 = 4
 num2 = 4
 num3 = num1*num2
-print(num3)
 
+print(num3)
