74b8d88315af25b72fd927d4e2d61b2c212257e7
kjkj <asddsf@dsafdsf.bg>
2019-02-08 17:09:58 +0200

    Week2

---
+++ Week_2/Modules/app.py
@@ -0,0 +1,7 @@
+# from Modules import my_funcs
+
+
+# my_funcs.add_number()
+
+
+# my_funcs.substract_number()
---
+++ Week_2/Modules/my_funcs.py
@@ -0,0 +1,10 @@
+# Create a module containing the following functions (add, substract) then
+#  import and use your module and then print the result
+
+
+def add_number(x, y):
+    return x + y
+
+
+def substract_number(x, y):
+    return x - y
